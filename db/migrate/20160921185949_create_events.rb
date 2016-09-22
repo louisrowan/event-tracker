4639aed029da3cff41d51045eb690c2557e46ba6
[01;31m[K"name"[m[K: "Louis Rowan <[01;31m[K"email"[m[K: null>
2016-09-21 22:44:37 -0700

    adds helper functions to parse

---
+++ db/migrate/20160921185949_create_events.rb
@@ -0,0 +1,16 @@
+class CreateEvents < ActiveRecord::Migration
+  def change
+    create_table :events do |t|
+      t.string :title
+      t.string :date
+      t.string :stadium
+      t.string :location
+      t.string :image
+      t.integer :ticket_count
+      t.float :lowest_price
+      t.string :buy_url
+
+      t.timestamps null: false
+    end
+  end
+end

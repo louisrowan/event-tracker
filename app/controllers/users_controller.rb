7a6bd5fcea03bdf31ff9ec72ec4ef021f1d590ca
Louis Rowan <email": null>
2016-09-24 12:00:32 -0700

    adds password hashing functionality

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,21 @@
+get '/users' do
+end
+
+get '/users/new' do
+  erb :'/users/new'
+end
+
+post '/users' do
+end
+
+get '/users/:id' do
+end
+
+get '/users/:d/edit' do
+end
+
+put '/users/:id' do
+end
+
+delete '/users/:id' do
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,15 @@
 class User < ActiveRecord::Base
+  include BCrypt
   has_and_belongs_to_many :events
+
+  def password
+    @password ||= Password.new(password_hash)
+  end
+
+  def password=(new_password)
+    @password = Password.create(new_password)
+    self.password_hash = @password
+  end
+
+
 end

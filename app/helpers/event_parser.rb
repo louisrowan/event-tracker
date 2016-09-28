17fbe9674eaf9338890dd2fa70636472fa2364ac
Louis Rowan <email": null>
2016-09-27 19:10:19 -0700

    adds giphy img search

--- app/helpers/event_parser.rb
+++ app/helpers/event_parser.rb
@@ -11,13 +11,13 @@ helpers do
 
 
 
-  def create_event_hash(event)
+  def create_event_hash(event, i, params)
     hash = {
       title: event["title"],
       date: Date.parse(event["datetime_local"]).to_s,
       stadium: event["venue"]["name"],
       location: event["venue"]["extended_address"],
-      image: event["performers"][0]["images"]["huge"],
+      image: add_giphy(params, i),
       ticket_count: event["stats"]["listing_count"].to_i || 0,
       lowest_price: event["stats"]["lowest_price"].to_i || 'N/A',
       buy_url: event["url"]
@@ -25,6 +25,11 @@ helpers do
       return hash
   end
 
-
+  def add_giphy(event, index)
+    event = event.split('-')
+    event = event.join('+')
+    response = HTTParty.get("http://api.giphy.com/v1/gifs/search?q=#{event}&api_key=dc6zaTOxFJmzC")
+    return response["data"][index]["images"]["fixed_height"]["url"]
+  end
 
 end
